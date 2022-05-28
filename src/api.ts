9570d84128d285b0fc4c25781754903a84288f73
Vladislav <vladfokin2002@gmail.com>
2022-05-28 19:21:15 +0300

    api update

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,9 @@ namespace api{
 
     export function defaultAction(req: any){
         switch(req.task){
-            case "get": return enc.encrypt(req.text || "test", "haha"); break;
+            case "get": return "hehe"; break;
+            case "enc": return enc.encrypt(req.text || "test", "haha"); break;
+            case "dec": return enc.decrypt(req.text || "test", "haha"); break;
         }
         return("api is loaded")
     }
