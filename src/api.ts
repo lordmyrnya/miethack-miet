266719a576e316e41d6bcf129901e2f3cf145891
KyuGG <zverolovz@mail.ru>
2022-05-28 19:46:54 +0300

    db test

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,19 @@
 import DB from "./db.js"
 import enc from "./enc.js"
 
-namespace api{
+namespace api {
 
-    export function defaultAction(req: any){
-        switch(req.task){
+    export async function defaultAction(req: any) {
+        switch (req.task) {
             case "get": return "hehe"; break;
             case "enc": return enc.encrypt(req.text || "test", "haha"); break;
             case "dec": return enc.decrypt(req.text || "test", "haha"); break;
+            case "db":
+                const result = JSON.stringify(await DB.getCorps())
+                return result
+                break
         }
-        return("api is loaded")
+        return ("api is loaded")
     }
 }
 
--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,12 @@
-namespace DB{
-
+import sqlite from 'sqlite-async'
+const db = await sqlite.open('./db/students.db')
+namespace DB {
+    export async function getCorps() {
+        // const sql = 'SELECT corpId,MAX(floor) FROM corps INNER JOIN rooms ON corps.corpId=rooms.corpId'
+        const sql = 'SELECT * FROM corps'
+        const result = await db.all(sql)
+        return result
+    }
 }
 
 export default DB
