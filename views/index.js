3c5f7727614566a0320219d46539608c36f5c1f5
KyuGG <zverolovz@mail.ru>
2022-05-28 21:51:15 +0300

    dynamically set floors

--- views/index.js
+++ views/index.js
@@ -1,6 +1,7 @@
 const modules = document.querySelectorAll('.module')
 const switchers = document.querySelectorAll('.switcher')
 const corpSelect = document.querySelector('.corp')
+const floorSelect = document.querySelector('.floor')
 
 //Загрузка корпусов
 const result = await(await fetch('http://localhost:3000/api/corps')).json()
@@ -12,8 +13,16 @@ for (const item of result) {
 }
 corpSelect.children[0].disabled = true
 
-corpSelect.onchange = () => {
-    console.log('object');
+corpSelect.onchange = async evt => {
+    while (floorSelect.children.length > 1)
+        floorSelect.lastChild.remove()
+    const floorsCount = await (await fetch(`http://localhost:3000/api/floors?corpId=${evt.target.value}`)).json()
+    for (let i = 1; i <= floorsCount['MAX(floor)']; i++) {
+        const floor = document.createElement('option')
+        floor.value = i
+        floor.textContent = `Этаж ${i}`
+        floorSelect.append(floor)
+    }
 }
 
 switchers.forEach(switcher => {
